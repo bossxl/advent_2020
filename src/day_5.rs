814b1e543128b76867d3a33381eb327620c63cf7
Paul Boss <bosspj@gmail.com>
2020-12-05 15:52:03 -0500

    Final day 5

---
+++ src/day_5.rs
@@ -0,0 +1,96 @@
+#![feature(destructuring_assignment)]
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Sen 1");
+    day_5(false);
+    // println!("Sen 2");
+    // day_four(true);
+}
+
+fn process_letter(letter: &str, low: i32, high: i32) -> (i32, i32) {
+  let mut new_low = low;
+  let mut new_high = high;
+  // let mid: i32 = ((high + low) as f32 / 2.0).ceil() as i32;
+  let mid: i32 = (high + low) / 2;
+  
+  println!("looking at [{}] l: {} h: {} m: {}", letter, low, high, mid);
+  match letter {
+    "F" => { new_high = mid }
+    "B" => { new_low = mid }
+    "L" => { new_high = mid }
+    "R" => { new_low = mid }
+    _ => { }
+  }
+  
+
+  return (new_low, new_high);
+}
+
+fn process_seat(seat_id: &str) -> i32 {
+  let mut high_row = 127;
+  let mut low_row = 0;
+  let mut high_col = 7;
+  let mut low_col = 0;
+  for letter in seat_id.split("") {
+    match letter {
+      "F" | "B" => { 
+        (low_row, high_row ) = process_letter(letter, low_row, high_row);
+        println!("result row at [{}] l: {} h: {}\n", letter, low_row, high_row);
+      }
+      "L" | "R" => { 
+        (low_col, high_col ) = process_letter(letter, low_col, high_col);
+        println!("result col at [{}] l: {} h: {}\n", letter, low_col, high_col);
+      }
+      _ => { }
+    }
+    
+  }
+  let result = high_row * 8 + high_col;
+  println!("result {}\n\n", result);
+  return result;
+}
+
+fn day_5(validate:bool) {
+  let filename = "/Users/pboss/workspace/advent/Y2020/data/day5.txt";
+  let contents = fs::read_to_string(filename)
+    .expect("Something went wrong reading the file");
+  let mut ids = vec![];
+  let max = contents
+    .lines()
+    .map(|f| {
+      let id = process_seat(f);
+      ids.push(id);
+      id
+    })
+    .max()
+    .unwrap_or(0);
+  println!("max {}", max);
+
+  ids.sort();
+  println!("ids {:?}", ids);
+  let mut last = 0;
+  let mut count = 0;
+  for id in ids.into_iter() {
+    if count == 0 {
+      count = id;
+      continue;
+    }
+    if last == 0 {
+      last = id;
+      count += 1;
+      continue;
+    }
+    println!("last {} id {} count {}", last, id, count);
+    if (last + 1) == id {
+      count += 1;
+      last = id;
+    } else {
+      println!("My Seat {}", count + 1);
+      break;
+    }
+
+  }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,16 @@ use std::fs;
 
 fn main() {
     println!("Hello, world!");
-    println!("Sen 1");
-    day_5(false);
-    // println!("Sen 2");
-    // day_four(true);
+    day_(false);
 }
 
-fn process_letter(letter: &str, low: i32, high: i32) -> (i32, i32) {
-  let mut new_low = low;
-  let mut new_high = high;
-  // let mid: i32 = ((high + low) as f32 / 2.0).ceil() as i32;
-  let mid: i32 = (high + low) / 2;
-  
-  println!("looking at [{}] l: {} h: {} m: {}", letter, low, high, mid);
-  match letter {
-    "F" => { new_high = mid }
-    "B" => { new_low = mid }
-    "L" => { new_high = mid }
-    "R" => { new_low = mid }
-    _ => { }
-  }
-  
-
-  return (new_low, new_high);
-}
-
-fn process_seat(seat_id: &str) -> i32 {
-  let mut high_row = 127;
-  let mut low_row = 0;
-  let mut high_col = 7;
-  let mut low_col = 0;
-  for letter in seat_id.split("") {
-    match letter {
-      "F" | "B" => { 
-        (low_row, high_row ) = process_letter(letter, low_row, high_row);
-        println!("result row at [{}] l: {} h: {}\n", letter, low_row, high_row);
-      }
-      "L" | "R" => { 
-        (low_col, high_col ) = process_letter(letter, low_col, high_col);
-        println!("result col at [{}] l: {} h: {}\n", letter, low_col, high_col);
-      }
-      _ => { }
-    }
-    
-  }
-  let result = high_row * 8 + high_col;
-  println!("result {}\n\n", result);
-  return result;
-}
-
-fn day_5(validate:bool) {
-  let mut total_valid = 0;
-  let mut total = 0;
+fn day_(validate:bool) {
   let filename = "/Users/pboss/workspace/advent/Y2020/data/day5.txt";
   let contents = fs::read_to_string(filename)
     .expect("Something went wrong reading the file");
-  
+  let mut ids = vec![];
   let max = contents
-    .lines()
-    .map(|f| process_seat(f) )
-    .max()
-    .unwrap_or(0);
-  println!("max {}", max);
-  
+    .lines();
+ 
 }
 
